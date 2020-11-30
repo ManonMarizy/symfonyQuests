c5b815aaea496c8aefd821260d235e9411d5def2
ManonMarizy <manon.marizy@gmail.com>
2020-11-30 19:35:18 +0100

    reglage quete 9

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -25,7 +25,7 @@ class Category
     private $name;
 
     /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Program", inversedBy="category", mappedBy="category")
+     * @ORM\OneToMany(targetEntity="App\Entity\Program", mappedBy="category")
      */
     private $programs;
 
