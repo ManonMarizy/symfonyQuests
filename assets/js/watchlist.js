2f20a3e15096cba3ca5c641df4c04213b3ee11c3
ManonMarizy <manon.marizy@gmail.com>
2021-01-25 10:30:09 +0100

    debug watchlist

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -1,17 +1,18 @@
 document.querySelector("#watchlist").addEventListener('click', addToWatchlist);
 
 function addToWatchlist(event) {
+    event.preventDefault();
 
 // Get the link object you click in the DOM
-    let watchlistIcon = event.target;
-    let link = watchlistIcon.dataset.text;
-    link = link.replace(/[^a-zA-Z\/\- ]/g, "");
+    let watchlistLink = event.currentTarget;
+    let link = watchlistLink.href;
 // Send an HTTP request with fetch to the URI defined in the href
     fetch(link)
         // Extract the JSON from the response
         .then(res => res.json())
         // Then update the icon
         .then(function (res) {
+            let watchlistIcon = watchlistLink.firstElementChild;
             if (res.isInWatchlist) {
                 watchlistIcon.classList.remove('far'); // Remove the .far (empty heart) from classes in <i> element
                 watchlistIcon.classList.add('fas'); // Add the .fas (full heart) from classes in <i> element
